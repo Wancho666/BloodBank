0db7f4875acea53ae648adecec14141403dc27ce
Bren666 <brenjustixx@gmail.com>
2025-04-21 20:56:29 +0800

    Test Run

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Ryzen 3\Desktop\BloodBankSystem\BloodBankSystem-Django\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
